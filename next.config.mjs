978cb703cbd5649748f918521d2c0f8098b735b9
unknown <gautama1512@gmail.com>
2025-12-10 17:52:28 +0530

    changes in package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-   images: {
-    domains: ["images.unsplash.com"], // ✅ allow unsplash images
+  images:{
+    unoptimized: true
   },
-  swcMinify: false,
-
+  experimental: {
+    swcWorker:1
+  }
 };
 
 export default nextConfig;
