075de9ad1b8d6018f97e9d3f3ea81993941000b4
gautamourya <gautama1512@gmail.com>
2026-02-15 19:27:15 +0530

    facebook analytics script added

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Providers from "./providers";
 import ScrollToTop from "../components/ScrollToTop";
 import { ToastContainer } from "react-toastify";
 import RazorpayScript from "../components/RazorpayScript";
+import Script from 'next/script';
 import "react-toastify/dist/ReactToastify.css";
 export const metadata = {
   title: "Talent Hunt",
@@ -13,6 +14,33 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Meta Pixel Code */}
+        <Script 
+          id="facebook-pixel"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1017587474768941');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          {`<img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=1017587474768941&ev=PageView&noscript=1"
+          />`}
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body className="bg-gray-50 text-gray-800" suppressHydrationWarning
       >
         <>
